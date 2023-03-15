ac025785418138bc593496625b430a6da3ac3c06
Diego Pereira <diegopereira1992d@gmail.com>
2023-03-15 14:11:46 -0300

    primeiro commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+
+int INDICE = 13, SOMA = 0, K = 0;
+
+   while(K < INDICE){
+
+       K = K + 1;
+
+       SOMA = SOMA + K;
+
+   }
+
+   printf("%d",SOMA);
+
+return 0;
+
+}
+
+
